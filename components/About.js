35b518eba336ad2c827176f6331f9529e17a0802
Tanav Poswal <63358333+tanavdev@users.noreply.github.com>
2023-12-22 17:43:56 +0530

    fix deploy err

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <div className="about-container">
       <h2>About Me</h2>
       <p>
-        I'm passionate about technology and coding! You can connect with me on GitHub and LinkedIn.
+        I&apos;m passionate about technology and coding! You can connect with me on GitHub and LinkedIn.
       </p>
       <p>
         GitHub: <a className="link" href="https://github.com/tanavdev" target="_blank" rel="noopener noreferrer">MY GitHub Profile</a>
